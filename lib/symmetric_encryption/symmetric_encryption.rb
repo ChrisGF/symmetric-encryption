891b149f662aea822c1b702cde4795c481cb7be3
Chris Schmitt <billsfannc@gmail.com>
2014-02-18 00:08:21 -0500

    - Added logging

--- lib/symmetric_encryption/symmetric_encryption.rb
+++ lib/symmetric_encryption/symmetric_encryption.rb
@@ -271,7 +271,11 @@ module SymmetricEncryption
   #
   # Existing key files will be renamed if present
   def self.generate_symmetric_key_files(filename=nil, environment=nil)
+    puts "Reading Config File: #{filename}  =>  ENV: #{environment}"
     config_filename = filename || File.join(Rails.root, "config", "symmetric-encryption.yml")
+    puts "Config File: #{config_filename}"
+    puts "Whole file: #{YAML.load(ERB.new(File.new(config_filename).read).result).inspect}"
+    
     config = YAML.load(ERB.new(File.new(config_filename).read).result)[environment || Rails.env]
 
     # RSA key to decrypt key files
